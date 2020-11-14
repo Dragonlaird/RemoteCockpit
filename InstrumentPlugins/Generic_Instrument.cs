91adb40e831901b2a81ed12b33d7a4044be30628
Jim Reeves <dragonlaird@msn.com>
2020-11-14 13:21:08 +0000

    Needs to refine animation to make it smoother but all works so far

--- InstrumentPlugins/Generic_Instrument.cs
+++ InstrumentPlugins/Generic_Instrument.cs
@@ -39,7 +39,7 @@ namespace InstrumentPlugins
         private int animationTimeInMs = 3000;
         private int animationStepCount = 10;
         private int animationUpdateInMs = 300;
-
+        private DateTime lastExecution = DateTime.MinValue;
         public Generic_Instrument()
         {
             config = new Configuration();
@@ -245,8 +245,8 @@ namespace InstrumentPlugins
                 StopTimer(timer);
             timer = new System.Timers.Timer(animationTimeInMs / animationStepCount);
             timer.Elapsed += ExecuteAnimation;
-            timer.AutoReset = true;
-            timer.Enabled = true;
+            //timer.AutoReset = true;
+            //timer.Enabled = true;
             timer.Start();
             if (timerIdx > -1)
                 animateTimers[timerIdx] = timer;
@@ -264,21 +264,22 @@ namespace InstrumentPlugins
             try
             {
                 var timerIdx = animateTimers.IndexOf((System.Timers.Timer)sender);
-                var animateTimer = (System.Timers.Timer)sender;
-                StopTimer(animateTimer);
+                lock (animateTimers)
+                    StopTimer(animateTimers[timerIdx]);
                 if (timerIdx > -1)
                 {
+                    var timeDiff = DateTime.Now.Subtract(lastExecution).TotalMilliseconds;
                     var lastValue = previousResults[timerIdx];
                     var currentValue = currentResults[timerIdx];
                     var stepSize = animationSteps[timerIdx];
                     lastValue.Result = (double)lastValue.Result + stepSize;
+                    lastExecution = DateTime.Now;
                     if (stepSize < 0)
                         if ((double)lastValue.Result < (double)currentValue.Result)
                         {
                             // Reached our limit
                             lastValue.Result = currentValue.Result;
                             RemoveTimer(timerIdx);
-                            animateTimer = null;
                         }
                         else
                         {
@@ -287,7 +288,6 @@ namespace InstrumentPlugins
                                 // Reached our limit
                                 lastValue.Result = currentValue.Result;
                                 RemoveTimer(timerIdx);
-                                animateTimer = null;
                             }
                         }
                     // Trigger paint event of any controls that rely on this value
@@ -297,9 +297,13 @@ namespace InstrumentPlugins
                         if (Control.Controls.ContainsKey(name))
                             UpdateInstrument(Control); // Force this control to repaint itself and any children
                     }
+                    if (timerIdx > -1)
+                        lock (animateTimers)
+                        {
+                            animateTimers[timerIdx] = new System.Timers.Timer(animationTimeInMs);
+                            StartTimer(animateTimers[timerIdx]);
+                        }
                 }
-                if (animateTimer != null)
-                    StartTimer(animateTimer);
             }
             catch { }
         }
@@ -316,7 +320,7 @@ namespace InstrumentPlugins
                 }
                 else
                 {
-                    PaintAnimation(obj, new PaintEventArgs(obj.CreateGraphics(), obj.DisplayRectangle));
+                    //PaintAnimation(obj, new PaintEventArgs(obj.CreateGraphics(), obj.DisplayRectangle));
                     obj.Invalidate(true);
                 }
             }
@@ -363,59 +367,6 @@ namespace InstrumentPlugins
                 return bitmap;
             }
             return null;
-        }
-
-        public PointF[] RemapPoints(AnimationPoint[] points, float absoluteX, float absoluteY, float imageSize, float angleInRadians)
-        {
-            List<PointF> remappedPoints = new List<PointF>();
-            var pixelsPerPercent = imageSize / 100;
-            for (var i = 0; i < points.Length; i++)
-            {
-                var nextPoint = points[i];
-                // Need to calculate the angle from origin for this point, then increment the angle by angleInRadians
-                //var lastPoint = i == 0 ? new AnimationPoint(0, 0) : points[i - 1];
-                //var deltaY = nextPoint.Y - lastPoint.Y;
-                //var deltaX = nextPoint.X - lastPoint.X;
-                //var pointAngle = (float)Math.Atan2(deltaY, deltaX) + angleInRadians;
-                nextPoint = new AnimationPoint(nextPoint.X * pixelsPerPercent, nextPoint.Y * pixelsPerPercent);
-                //remappedPoints.Add(GetPoint(nextPoint, absoluteX, absoluteY, pointAngle));
-                var newPoint = CartesianWithAngularOffset(nextPoint.Point, angleInRadians);
-                newPoint.X += absoluteX;
-                newPoint.Y += absoluteY;
-                remappedPoints.Add(newPoint);
-            }
-            return remappedPoints.ToArray();
-        }
-
-        private float GetPointAngle(PointF startPoint, PointF destPoint)
-        {
-            float xDiff = destPoint.X - startPoint.X;
-            float yDiff = destPoint.Y - startPoint.Y;
-            //return Math.Atan2(yDiff, xDiff) * 180.0 / Math.PI;
-            var pointAngle = Math.Atan2(yDiff, xDiff);
-            return (float)pointAngle;
-        }
-
-        private float GetPointRadius(PointF point)
-        {
-            var radius = Math.Sqrt(point.X * point.X + point.Y * point.Y);
-            return (float)radius;
-        }
-
-        private PointF CartesianWithAngularOffset(PointF point, float angleInRadians)
-        {
-            var pointAngle = GetPointAngle(new PointF { X = 0, Y = 0 }, point);
-            var radius = GetPointRadius(point);
-            var newAngle = pointAngle + angleInRadians;
-            return PolarToCartesian(newAngle, radius);
-        }
-
-        private PointF PolarToCartesian(double angle, double radius)
-        {
-            double angleRad = (Math.PI / 180.0) * (angle - 90);
-            double x = radius * Math.Cos(angleRad);
-            double y = radius * Math.Sin(angleRad);
-            return new PointF((float)x, (float)y);
         }
 
         public void LoadConfigFile(string filePath)
@@ -486,108 +437,12 @@ namespace InstrumentPlugins
                     // This will cause ExecuteAnimation to run, which should update the Last Value by Step Size & trigger Paint Event for any controls relying on this variable to update
                     lock (animateTimers)
                     {
+                        lastExecution = DateTime.Now;
                         animateTimers[resultIdx] = new System.Timers.Timer(animationTime);
-                        animateTimers[resultIdx] = StartTimer(animateTimers[resultIdx]);
+                        StartTimer(animateTimers[resultIdx]);
                     }
                 }
             }
-        }
-
-        /// <summary>
-        /// Method to rotate an Image object. The result can be one of three cases:
-        /// - upsizeOk = true: output image will be larger than the input, and no clipping occurs 
-        /// - upsizeOk = false & clipOk = true: output same size as input, clipping occurs
-        /// - upsizeOk = false & clipOk = false: output same size as input, image reduced, no clipping
-        /// 
-        /// A background color must be specified, and this color will fill the edges that are not 
-        /// occupied by the rotated image. If color = transparent the output image will be 32-bit, 
-        /// otherwise the output image will be 24-bit.
-        /// 
-        /// Note that this method always returns a new Bitmap object, even if rotation is zero - in 
-        /// which case the returned object is a clone of the input object. 
-        /// </summary>
-        /// <param name="inputImage">input Image object, is not modified</param>
-        /// <param name="angleRadians">angle of rotation, in radians</param>
-        /// <param name="upsizeOk">see comments above</param>
-        /// <param name="clipOk">see comments above, not used if upsizeOk = true</param>
-        /// <param name="backgroundColor">color to fill exposed parts of the background</param>
-        /// <returns>new Bitmap object, may be larger than input image</returns>
-        private Bitmap RotateImage(Image inputImage, float angleRadians, bool upsizeOk,
-                                         bool clipOk, Color backgroundColor)
-        {
-            // Test for zero rotation and return a clone of the input image
-            if (angleRadians == 0f)
-                return (Bitmap)inputImage.Clone();
-
-            // Set up old and new image dimensions, assuming upsizing not wanted and clipping OK
-            float oldWidth = inputImage.Width;
-            float oldHeight = inputImage.Height;
-            int newWidth = (int)oldWidth;
-            int newHeight = (int)oldHeight;
-            float scaleFactor = 1f;
-
-            // If upsizing wanted or clipping not OK calculate the size of the resulting bitmap
-            if (upsizeOk || !clipOk)
-            {
-                //double angleRadians = angleDegrees * Math.PI / 180d;
-
-                double cos = Math.Abs(Math.Cos(angleRadians));
-                double sin = Math.Abs(Math.Sin(angleRadians));
-                newWidth = (int)Math.Round(oldWidth * cos + oldHeight * sin);
-                newHeight = (int)Math.Round(oldWidth * sin + oldHeight * cos);
-            }
-
-            // If upsizing not wanted and clipping not OK need a scaling factor
-            if (!upsizeOk && !clipOk)
-            {
-                scaleFactor = Math.Min((float)oldWidth / newWidth, (float)oldHeight / newHeight);
-                newWidth = (int)oldWidth;
-                newHeight = (int)oldHeight;
-            }
-
-            // Create the new bitmap object. If background color is transparent it must be 32-bit, 
-            //  otherwise 24-bit is good enough.
-            Bitmap newBitmap = new Bitmap((int)newWidth, (int)newHeight, backgroundColor == Color.Transparent ?
-                                             PixelFormat.Format32bppArgb : PixelFormat.Format24bppRgb);
-            newBitmap.SetResolution(inputImage.HorizontalResolution, inputImage.VerticalResolution);
-
-            // Create the Graphics object that does the work
-            using (Graphics graphicsObject = Graphics.FromImage(newBitmap))
-            {
-
-                graphicsObject.InterpolationMode = InterpolationMode.HighQualityBicubic;
-                graphicsObject.PixelOffsetMode = PixelOffsetMode.HighQuality;
-                graphicsObject.SmoothingMode = SmoothingMode.HighQuality;
-
-                // Fill in the specified background color if necessary
-                if (backgroundColor != Color.Transparent)
-                    graphicsObject.Clear(backgroundColor);
-
-                // Set up the built-in transformation matrix to do the rotation and maybe scaling
-                Matrix matrix = new Matrix();
-
-                // Rotate about image center point
-                matrix.RotateAt(20, new PointF(newWidth / 2f, newHeight / 2f));
-                var transformX = oldWidth / 2;
-                var transformY = oldHeight / 2;
-                matrix.Translate(transformX, transformY);
-                // Move the transform point back to the top left
-                //graphicsObject.TranslateTransform(-1*(newWidth - oldWidth), -1*(newHeight - oldHeight));
-
-                graphicsObject.Transform = matrix;
-                graphicsObject.DrawImage(newBitmap, new Point());
-                //graphicsObject.TranslateTransform(newWidth / 2f, newHeight / 2f);
-                if (scaleFactor != 1f)
-                    graphicsObject.ScaleTransform(scaleFactor, scaleFactor);
-
-                //graphicsObject.RotateTransform(angleRadians);
-                graphicsObject.TranslateTransform(-oldWidth / 2f, -oldHeight / 2f);
-
-                // Draw the result 
-                graphicsObject.DrawImage(inputImage, 0, 0);
-            }
-
-            return newBitmap;
         }
 
         private Image ClipToCircle(Image srcImage, PointF center, float radius, Color backGround)
