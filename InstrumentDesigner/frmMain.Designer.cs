bbda228f17c13cbc16802f9c84d94e0092f934ff
Jim Reeves <dragonlaird@msn.com>
2020-11-16 08:17:08 +0000

    Start loading config file and display settings

--- InstrumentDesigner/frmMain.Designer.cs
+++ InstrumentDesigner/frmMain.Designer.cs
@@ -38,12 +38,17 @@
             this.fileToolStripMenuItemSep = new System.Windows.Forms.ToolStripSeparator();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.mnuBackground = new System.Windows.Forms.ToolStripMenuItem();
-            this.importImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.defaultColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.resizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.defaultColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.lblInstrumentName = new System.Windows.Forms.Label();
+            this.txtInstrumentName = new System.Windows.Forms.TextBox();
+            this.pbBackgroundImage = new System.Windows.Forms.PictureBox();
+            this.lblBackgroundImage = new System.Windows.Forms.Label();
             this.mnuMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBackgroundImage)).BeginInit();
             this.SuspendLayout();
             // 
             // mnuMain
@@ -74,44 +79,45 @@
             // newToolStripMenuItem
             // 
             this.newToolStripMenuItem.Name = "newToolStripMenuItem";
-            this.newToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
             this.newToolStripMenuItem.Text = "&New";
             // 
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
             this.openToolStripMenuItem.Text = "&Open";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.LoadConfig);
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
             this.saveToolStripMenuItem.Text = "&Save";
             // 
             // saveAsToolStripMenuItem
             // 
             this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
-            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
             this.saveAsToolStripMenuItem.Text = "Save &As";
             // 
             // closeToolStripMenuItem
             // 
             this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
-            this.closeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
             this.closeToolStripMenuItem.Text = "&Close";
             // 
             // fileToolStripMenuItemSep
             // 
             this.fileToolStripMenuItemSep.Name = "fileToolStripMenuItemSep";
-            this.fileToolStripMenuItemSep.Size = new System.Drawing.Size(177, 6);
+            this.fileToolStripMenuItemSep.Size = new System.Drawing.Size(111, 6);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
             this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.CloseForm);
             // 
             // mnuBackground
             // 
@@ -124,46 +130,86 @@
             this.mnuBackground.Size = new System.Drawing.Size(83, 20);
             this.mnuBackground.Text = "&Background";
             // 
-            // importImageToolStripMenuItem
-            // 
-            this.importImageToolStripMenuItem.Name = "importImageToolStripMenuItem";
-            this.importImageToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.importImageToolStripMenuItem.Text = "&Import Image";
-            // 
-            // defaultColorToolStripMenuItem
-            // 
-            this.defaultColorToolStripMenuItem.Name = "defaultColorToolStripMenuItem";
-            this.defaultColorToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.defaultColorToolStripMenuItem.Text = "Default &Color";
-            // 
             // clearToolStripMenuItem
             // 
             this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
-            this.clearToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
             this.clearToolStripMenuItem.Text = "C&lear";
             // 
             // resizeToolStripMenuItem
             // 
             this.resizeToolStripMenuItem.Name = "resizeToolStripMenuItem";
-            this.resizeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.resizeToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
             this.resizeToolStripMenuItem.Text = "&Resize";
+            // 
+            // importImageToolStripMenuItem
+            // 
+            this.importImageToolStripMenuItem.Name = "importImageToolStripMenuItem";
+            this.importImageToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.importImageToolStripMenuItem.Text = "&Import Image";
+            // 
+            // defaultColorToolStripMenuItem
+            // 
+            this.defaultColorToolStripMenuItem.Name = "defaultColorToolStripMenuItem";
+            this.defaultColorToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.defaultColorToolStripMenuItem.Text = "Default &Color";
             // 
             // openFileDialog
             // 
             this.openFileDialog.FileName = "openFileDialog";
             this.openFileDialog.Filter = "Instrument Configurations|*.json";
             // 
+            // lblInstrumentName
+            // 
+            this.lblInstrumentName.AutoSize = true;
+            this.lblInstrumentName.Location = new System.Drawing.Point(13, 28);
+            this.lblInstrumentName.Name = "lblInstrumentName";
+            this.lblInstrumentName.Size = new System.Drawing.Size(90, 13);
+            this.lblInstrumentName.TabIndex = 1;
+            this.lblInstrumentName.Text = "Instrument Name:";
+            this.lblInstrumentName.Click += new System.EventHandler(this.focusInstrumentName);
+            // 
+            // txtInstrumentName
+            // 
+            this.txtInstrumentName.Location = new System.Drawing.Point(109, 25);
+            this.txtInstrumentName.Name = "txtInstrumentName";
+            this.txtInstrumentName.Size = new System.Drawing.Size(297, 20);
+            this.txtInstrumentName.TabIndex = 2;
+            // 
+            // pbBackgroundImage
+            // 
+            this.pbBackgroundImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbBackgroundImage.Location = new System.Drawing.Point(16, 75);
+            this.pbBackgroundImage.Name = "pbBackgroundImage";
+            this.pbBackgroundImage.Size = new System.Drawing.Size(339, 272);
+            this.pbBackgroundImage.TabIndex = 3;
+            this.pbBackgroundImage.TabStop = false;
+            // 
+            // lblBackgroundImage
+            // 
+            this.lblBackgroundImage.AutoSize = true;
+            this.lblBackgroundImage.Location = new System.Drawing.Point(16, 56);
+            this.lblBackgroundImage.Name = "lblBackgroundImage";
+            this.lblBackgroundImage.Size = new System.Drawing.Size(100, 13);
+            this.lblBackgroundImage.TabIndex = 4;
+            this.lblBackgroundImage.Text = "Background Image:";
+            // 
             // frmInstrumentDesigner
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1193, 552);
+            this.Controls.Add(this.lblBackgroundImage);
+            this.Controls.Add(this.pbBackgroundImage);
+            this.Controls.Add(this.txtInstrumentName);
+            this.Controls.Add(this.lblInstrumentName);
             this.Controls.Add(this.mnuMain);
             this.MainMenuStrip = this.mnuMain;
             this.Name = "frmInstrumentDesigner";
             this.Text = "Instrument Designer";
             this.mnuMain.ResumeLayout(false);
             this.mnuMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBackgroundImage)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -186,6 +232,10 @@
         private System.Windows.Forms.ToolStripMenuItem importImageToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem defaultColorToolStripMenuItem;
         private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Label lblInstrumentName;
+        private System.Windows.Forms.TextBox txtInstrumentName;
+        private System.Windows.Forms.PictureBox pbBackgroundImage;
+        private System.Windows.Forms.Label lblBackgroundImage;
     }
 }
 
--- InstrumentDesigner/frmMain.cs
+++ InstrumentDesigner/frmMain.cs
@@ -16,6 +16,8 @@ namespace InstrumentDesigner
     public partial class frmInstrumentDesigner : Form
     {
         private Configuration config;
+        private string configFilePath;
+        private string cockpitDirectory;
         public frmInstrumentDesigner()
         {
             InitializeComponent();
@@ -25,9 +27,56 @@ namespace InstrumentDesigner
         private void Initialize()
         {
             config = new Configuration();
+            configFilePath = "";
             openFileDialog.FileName = "";
             openFileDialog.InitialDirectory = Directory.GetCurrentDirectory();
 
+        }
+
+        private void PopulateConfigForm()
+        {
+            if(config == null)
+            {
+                config = new Configuration();
+            }
+            txtInstrumentName.Text = config.Name;
+            var image = LoadImage(Path.Combine(cockpitDirectory, config.BackgroundImagePath));
+            if(image != null)
+            {
+                ShowImage(image, pbBackgroundImage);
+            }
+        }
+
+        private void ShowImage(Image image, Control imageBox)
+        {
+            var imageScaleFactor = (double)imageBox.Width / image.Width;
+            var aspectRatio = (double)image.Height / image.Width;
+            if (image.Height * imageScaleFactor > imageBox.Height)
+                imageScaleFactor = (double)imageBox.Height / image.Height;
+            var backgroundImage = new Bitmap(imageBox.Width, imageBox.Height);
+            using (Graphics gr = Graphics.FromImage(backgroundImage))
+            {
+                gr.DrawImage(new Bitmap(image, new Size((int)(image.Width * imageScaleFactor), (int)(image.Height * imageScaleFactor))), new Point(0, 0));
+            }
+            imageBox.BackColor = Color.AliceBlue; // Use this to show where image doesn't fit the control
+            imageBox.BackgroundImage = backgroundImage;
+        }
+
+        private Image LoadImage(string imagePath)
+        {
+            Image image = null;
+            try
+            {
+                using (var fileStream = File.OpenRead(imagePath)) {
+                    image = Image.FromStream(fileStream);
+                    fileStream.Close();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(string.Format("Unable to load Image:\r\r{0}",imagePath), "Load File Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return image;
         }
 
         private void LoadConfig(object sender, EventArgs e)
@@ -44,6 +93,10 @@ namespace InstrumentDesigner
                     {
                         throw new Exception("Configuration Load Failed");
                     }
+                    configFilePath = fileName;
+                    cockpitDirectory = Path.Combine(Path.GetDirectoryName(configFilePath), "..");
+                    openFileDialog.InitialDirectory = Path.GetDirectoryName(configFilePath);
+                    PopulateConfigForm();
                 }
                 catch(IOException ex)
                 {
@@ -57,6 +110,17 @@ namespace InstrumentDesigner
                 }
             }
 
+        }
+
+        private void focusInstrumentName(object sender, EventArgs e)
+        {
+            txtInstrumentName.Focus();
+        }
+
+        private void CloseForm(object sender, EventArgs e)
+        {
+            this.FindForm().Close();
+            this.FindForm().Dispose();
         }
     }
 }
