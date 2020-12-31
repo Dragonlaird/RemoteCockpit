e44f9618c751d949544c5e8236aaf534a08e84f3
Jim Reeves <dragonlaird@msn.com>
2020-12-31 08:20:32 +0000

    Added installer project

--- FSRemoteCockpitServer/Properties/AssemblyInfo.cs
+++ FSRemoteCockpitServer/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RemoteCockpitServer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Remote Service API for MSFS")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Dragonlaird")]
 [assembly: AssemblyProduct("RemoteCockpit")]
 [assembly: AssemblyCopyright("Copyright ©  2020 - Dragonlaird")]
 [assembly: AssemblyTrademark("")]
--- InstrumentDesigner/Properties/AssemblyInfo.cs
+++ InstrumentDesigner/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("InstrumentDesigner")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Desktop application to design Cockpit Instruments for MSFS Cockpit")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Dragonlaird")]
 [assembly: AssemblyProduct("InstrumentDesigner")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
